74890abb7d0125470b0b6f2f6c79826461ac0c18
Puyan Wei <puyanwei@gmail.com>
2023-07-02 13:26:44 +0100

    Add scoring logic

--- src/components/pages/QuizPage/index.tsx
+++ src/components/pages/QuizPage/index.tsx
@@ -13,12 +13,24 @@ interface QuizPageProps extends Component {}
 
 export function QuizPage({ className, testId = "quiz-page" }: QuizPageProps) {
   const [currentQuestion, setCurrentQuestion] = useState(1)
+  const [score, setScore] = useState(0)
 
   if (isEmptyObject(data)) throw new Error("Data object is empty")
 
-  function handleOnClick() {
+  function handleClick(isCorrect: boolean) {
+    if (isCorrect) setScore(score + 1)
     setCurrentQuestion(currentQuestion + 1)
   }
+
+  if (currentQuestion > data.length)
+    return (
+      <main className={`grid place-items-center h-screen ${className}`} data-testid={testId}>
+        <Card className="flex flex-col space-y-4">
+          <Heading>Quiz Completed</Heading>
+          <Text>{`Your final score is ${score}/${data.length}`}</Text>
+        </Card>
+      </main>
+    )
 
   const { question, answers } = data[currentQuestion - 1]
 
@@ -28,10 +40,9 @@ export function QuizPage({ className, testId = "quiz-page" }: QuizPageProps) {
         <Heading>Question {currentQuestion}</Heading>
         <Text>{question}</Text>
         <div className="flex flex-grow" />
-        {answers.map(({ phrasing }) => (
-          <Text>{phrasing}</Text>
+        {answers.map(({ phrasing, isCorrect }) => (
+          <Button onClick={() => handleClick(isCorrect)}>{phrasing}</Button>
         ))}
-        <Button onClick={handleOnClick}>Submit</Button>
         <div className="pb-8"></div>
       </Card>
     </main>
